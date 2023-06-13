d91bbebf7b6d1784875e0cea1975632570500f04
Brent Hopkins <contact@brenthopkins.me>
2023-06-12 20:16:48 -0500

    Return userRoutes.js to original state

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,23 +6,22 @@ const pictureController = require('../controllers/pictureController');
 const { getProfilePicture, addProfilePicture, upload } = pictureController; 
 
 
-// form-data, x-www-urlencoded, JSON supported except for Twilio/Sendgrid which requires x-www-form-urlencoded or JSON
 router.route('/register')
-    .post(userController.determineRequestType, userController.handleNewUser)
-    .post(userController.determineRequestType, sendGrid.emailNewUser)
+    .post(userController.handleNewUser)
+    .post(sendGrid.emailNewUser)
     
 router.route('/auth')
     .get(userController.handleLogout)
-    .post(userController.determineRequestType, userController.handleLogin)
+    .post(userController.handleLogin)
 
 router.route('/refresh')
     .get(userController.handleRefreshToken)
 
 router.route('/twofactor')
-    .post(userController.determineRequestType, sendGrid.TwoFAEmail)
+    .post(sendGrid.TwoFAEmail)
 
 router.route('/twofactorconfirm')
-    .post(userController.determineRequestType, sendGrid.ConfirmTwoFAEmail)    
+    .post(sendGrid.ConfirmTwoFAEmail)    
 
 router.route('/profilepicture')
     .post(upload, addProfilePicture)
