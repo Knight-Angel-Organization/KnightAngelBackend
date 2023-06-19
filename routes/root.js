e556449ca2dd56f21d65f0b95ff187bc8332c266
Xavier Baldwin <xdb19981@gmail.com>
2023-06-18 23:42:11 -0500

    Code Beautification
    cleaned up some blank space in code and removed non-needed files

--- routes/root.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const authController = require('../controllers/authController');
-
-router.post('/', authController.handleLogin)
-
-module.exports = router
