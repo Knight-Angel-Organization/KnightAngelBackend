8d32536a6a366f59d63d9c531c49ac1deb3d76f4
Xavier Baldwin <xdb19981@gmail.com>
2023-09-12 19:26:55 -0500

    Re-arrange
    Moved file & updated dependencies.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const sendGrid = require('../utils/sendGrid');
-const pictureController = require('../controllers/pictureController');
+const pictureController = require('../utils/pictureController');
 
 
 router.route('/register')
