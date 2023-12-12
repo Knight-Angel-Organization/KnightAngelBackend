eb7ee45b0339ce4914d8d7d3791af5a8490f4563
Dinesh Chhantyal <dineshchhantyal@Dineshs-Laptop.local>
2023-12-11 23:21:25 -0600

    fix: change profilePic type as ObjectId

--- controllers/postController.js
+++ controllers/postController.js
@@ -23,23 +23,23 @@ To-do:
 - Add a way to add comments to posts.
 - Add a way to delete comments.
 - Add a way to edit comments.
-- Maybe add a way to like posts.
+123- Maybe add a way to like posts.
 - Maybe add a way to like comments.
 
 
 */
 
 
-const addPost = asyncHandler(async (req, res) => { 
+const addPost = asyncHandler(async (req, res) => {
 
-  // Get the user ID + post content, title, location and type from the request body. It is optional to have the location.
-  const _userID = req.body.userID;
-  const _postContent = req.body.postContent;
-  const _postTitle = req.body.postTitle;
-  const _postType = req.body.postType;
-  const _postCategory = req.body.postCategory;
-  let _postLocation;
-  // Check if post location is provided, if not, set it to "false".
+    // Get the user ID + post content, title, location and type from the request body. It is optional to have the location.
+    const _userID = req.body.userID;
+    const _postContent = req.body.postContent;
+    const _postTitle = req.body.postTitle;
+    const _postType = req.body.postType;
+    const _postCategory = req.body.postCategory;
+    let _postLocation;
+    // Check if post location is provided, if not, set it to "false".
     if (!req.body.postLocation) {
         _postLocation = "false";
     } else {
@@ -49,11 +49,11 @@ const addPost = asyncHandler(async (req, res) => {
 
     // Check if the user ID, post content, title, category and type are provided, if not, return an error message.
 
-  if (!_postContent || !_postTitle || !_postType || !_postCategory || !_userID) {
-    return res.status(400).json({ 'message': 'Error: Post content, title, category, type, and user ID are required.' });
-  }
+    if (!_postContent || !_postTitle || !_postType || !_postCategory || !_userID) {
+        return res.status(400).json({ 'message': 'Error: Post content, title, category, type, and user ID are required.' });
+    }
 
-  // If post content is longer than 1000 characters or post title is longer than 100 characters.
+    // If post content is longer than 1000 characters or post title is longer than 100 characters.
     if (_postContent.length > 1000) {
         return res.status(400).json({ 'message': 'Error: Post content is too long. (1000 characters maximum)' });
     } else if (_postTitle.length > 100) {
@@ -132,10 +132,10 @@ const getPost = asyncHandler(async (req, res) => {
 
 /*
 const addComment = asyncHandler(async (req, res) => {
-    
+
     // Get the post ID from the request body.
     const _postID = req.body.postID;
-    
+
     // Get the user ID + comment content from the request body.
     const _userID = req.body.userID;
     const _commentContent = req.body.commentContent;
@@ -255,8 +255,8 @@ const likePost = asyncHandler(async (req, res) => {
     if (!foundPost) {
         return res.status(400).json({ 'message': 'Error: Post ID not found.' });
     }
-    
-    
+
+
     if (foundPost.postLikes.includes(_userID)) {
         return res.status(400).json({ 'message': 'Error: You have already liked this post.' });
     }
--- model/User.js
+++ model/User.js
@@ -1,51 +1,51 @@
 const mongoose = require("mongoose");
 const ObjectId = mongoose.ObjectId;
 const imageSchema = require('./Image');
-// Making a user schema with all the details required from the user 
+// Making a user schema with all the details required from the user
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   firstName: {
     type: String,
     required: false,
-    },
-  lastName:{
+  },
+  lastName: {
     type: String,
     required: false
-    },
+  },
   username: {
     type: String,
     required: true,
     unique: true,
-    },
+  },
   email: {
     type: String,
     required: true,
     unique: true,
-    },
+  },
   isVerified: {
     type: Boolean,
     default: false,
     //to check if the account is verified or not
-    },
+  },
   password: {
     type: String,
     required: true,
-    },
+  },
   date: {
     type: Date,
     default: Date.now, //date will be set to the time the account is created
-    },
+  },
   followers: {//ppl that follow the user
     type: Array,
     default: [],
-    },
+  },
   followings: {// ppl the user is following
     type: Array,
     default: [],
-    },
+  },
   refreshToken: [String], //having in array supports multidevice login
-  profilePic: imageSchema,
+  profilePic: Schema.ObjectId,// imageSchema id
   emergencyContacts: [String]
 })
 
