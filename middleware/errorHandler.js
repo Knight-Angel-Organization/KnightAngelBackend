9e6a7fda13397c1f18700b199c31fedab0010704
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2023-12-28 19:11:17 -0600

    Fix error handling and add notFound middleware

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,27 @@
-const {logEvents} =require('./logEvents');
+const { logEvents } = require('./logEvents');
 
-const errorHandler = (err, req, res, next) =>{
+export function notFound(req, res, next) {
+    res.status(404);
+    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+    next(error);
+}
+
+const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.txt')
     console.error(err.stack)
-    res.status(500).send(err.message);
+    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+    res.status(statusCode);
+
+    if (process.env.NODE_ENV === 'production') {
+        res.json({
+            message: err.message,
+        });
+    } else {
+        res.json({
+            message: err.message,
+            stack: err.stack,
+        });
+    }
 }
 
 module.exports = errorHandler;
