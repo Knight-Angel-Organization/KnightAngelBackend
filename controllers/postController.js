7815a78c6dd466f3f9334689d218f53a798f7661
Xavier Baldwin <xdb19981@gmail.com>
2024-06-03 22:49:50 -0500

    Organizing controllers
    Moved add comments.

--- controllers/postController.js
+++ controllers/postController.js
@@ -154,53 +154,6 @@ const getPost = asyncHandler(async (req, res) => {
 });
 
 
-const addComment = asyncHandler(async (req, res) => {
-
-    const _postID = req.body.postID;
-    const _userID = req.body.userID;
-    const _commentContent = req.body.commentContent;
-
-    if (!_postID || !_userID || !_commentContent) {
-        return res.status(400).json({ 'message': 'Error: Post ID, user ID, and comment content are required.' });
-    }
-
-    // Verify post ID and user ID is a string of 24 hex characters
-    if (!/^[0-9a-fA-F]{24}$/.test(_postID) || !/^[0-9a-fA-F]{24}$/.test(_userID)) {
-        return res.status(400).json({ 'message': 'Post ID or user ID is invalid.' });
-    }
-
-    // Check if the post ID is in database
-    const foundPost = await Post.findOne({ _id: new ObjectId(_postID) });
-
-    if (!foundPost) {
-        return res.status(400).json({ 'message': 'Error: Post ID is invalid.' });
-    }
-
-    // Check if the user ID exists
-    const foundUser = await User.findOne({ _id: new ObjectId(_userID) });
-
-    // Keep same error message to prevent username enumeration
-    if (!foundUser) {
-        return res.status(400).json({ 'message': 'Post ID or user ID is invalid.' });
-    }
-
-    const newComment = {
-        postID: _postID,
-        username: foundUser.username,
-        commentContent: _commentContent,
-        commentReactions: []
-    };
-
-    const addedComment = await Comment.create(newComment);
-
-    if (addedComment) {
-        return res.status(201).json({ 'success': 'Comment added successfully.' });
-    } else {
-        return res.status(400).json({ 'message': 'Error: Comment could not be added.' });
-    }
-
-});
-
 
 const editPost = asyncHandler(async (req, res) => {
 
